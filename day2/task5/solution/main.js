96f484d43664ce8e5ebbffea78cd9cbde35fa36b
Gert Hengeveld <info@ghengeveld.nl>
2017-04-14 15:41:53 +0200

    Proper naming.

--- day2/task5/solution/main.js
+++ day2/task5/solution/main.js
@@ -2,7 +2,7 @@ const flow = (...fns) => x => fns.reduce((v, fn) => fn(v), x);
 
 const toLowerCase = str => str.toLowerCase();
 const makeSplitter = separator => str => str.split(separator);
-const makeJoiner = separator => str => str.join(separator);
+const makeJoiner = separator => arr => arr.join(separator);
 
 const slugify = flow(
   toLowerCase,
