571bedfecd11e016a1b4efa519d528b7090e584a
Gert Hengeveld <info@ghengeveld.nl>
2019-01-09 09:36:22 +0100

    Update all dependencies to fix security issues.

--- day2/task6/webpack.config.js
+++ day2/task6/webpack.config.js
@@ -1,29 +1,25 @@
-const path = require('path');
-// const BabiliPlugin = require('babili-webpack-plugin');
-const HtmlwebpackPlugin = require('html-webpack-plugin');
+const path = require("path")
+const HtmlwebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
-  target: 'web',
-  entry: './main.js',
+  target: "web",
+  entry: "./main.js",
   // entry: './solution/main.js',
-  devtool: 'source-map',
+  devtool: "source-map",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
-    publicPath: '/static/'
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    publicPath: "/static/",
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
-    ]
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      { test: /\.html$/, use: [{ loader: "html-loader" }] },
+    ],
   },
   plugins: [
-    // new BabiliPlugin({
-    //   deadcode: true,
-    //   mangle: true
-    // })
     new HtmlwebpackPlugin({
-      title: 'Day2/Task2'
-    })
-  ]
-};
+      title: "Day2/Task6",
+    }),
+  ],
+}
